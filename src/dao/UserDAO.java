a666ba389f42c0e8463db34fb193d056048a1f23
rycamosun <ryselloooo@gmail.com>
2026-04-18 14:29:31 -0700

    feat: added UserDAO

---
+++ src/dao/UserDAO.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import model.User;
+import util.DBConnection;
+import java.sql.*;
+
+/**
+ *
+ * @author ry
+ */
+public class UserDAO {
+    
+    /**
+     * 
+     * @param user
+     * @return 
+     */
+    public boolean registerUser(User user) {
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+ 
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            return ps.executeUpdate() > 0;
+ 
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    /**
+     * 
+     * @param username
+     * @return 
+     */
+    public User findByUsername(String username) {
+        String sql = "SELECT * FROM users WHERE username = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+ 
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                User user = new User();
+                user.setUserId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+ 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
